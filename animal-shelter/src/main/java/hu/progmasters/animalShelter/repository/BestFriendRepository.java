f2c25d95de8f60eed5e3ff7e314de9919a676788
MrGyevad <69209232+MrGyevad@users.noreply.github.com>
2021-08-11 15:35:04 +0200

    Update BestFriendRepository.java

--- animal-shelter/src/main/java/hu/progmasters/animalShelter/repository/BestFriendRepository.java
+++ animal-shelter/src/main/java/hu/progmasters/animalShelter/repository/BestFriendRepository.java
@@ -12,15 +12,34 @@ public class BestFriendRepository {
 
     @PersistenceContext
     private EntityManager entityManager;
+    
+    private Cat cat;
+    private Dog dog;
+    
+    public BestFriend becomeBestFriends(Cat cat, Dog dog){
+        BestFriend toSave = new BestFriend(Cat cat, Dog dog);
+        BestFriend saved = entityManager.persist(toSave);
+        cat.setBestFriendId(saved.getId());
+        dog.setBestFriendId(saved.getId());
+        return saved;
+    }
+    
+    public BestFriend findFriendshipById(Integer id){
+        return entityManager.find(BestFriend.class, id);
+    }
 
-    public void saveAdoptedCat(Cat cat, Dog dog) {
-        entityManager.persist(dog);
-        entityManager.persist(cat);
+    public void saveAdoptedCat(Cat cat) {
+        BestFriend friendship = findFriendshipById(cat.getBestFriendId());
+        
     }
 
     public void saveAdoptedDog(Dog dog, Cat cat) {
         entityManager.persist(dog);
         entityManager.persist(cat);
+    }
+    
+    public List<BestFriend> findAll(){
+        return entityManager.createQuery("SELECT b FROM BestFriend b", BestFriend.class).getResultList();
     }
 
 
