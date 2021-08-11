ffcf6d6272283b96ab279ac3f8f9de3417f9d3fa
MrGyevad <69209232+MrGyevad@users.noreply.github.com>
2021-08-11 12:41:42 +0200

    Update DogRepositoryTest.java

--- animal-shelter/src/test/java/hu/progmasters/animalShelter/DogRepositoryTest.java
+++ animal-shelter/src/test/java/hu/progmasters/animalShelter/DogRepositoryTest.java
@@ -25,17 +25,8 @@ public class DogRepositoryTest {
     @Order(1)
     @Transactional
     void testSaveDog_successfulSave(){
-        String ldt = "2021-08-03 15:40:00";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime dateTime = LocalDateTime.parse(ldt, formatter);
-        Dog toSave = new Dog();
-        toSave.setLastWalk(dateTime);
-        toSave.setHasWaterAndFood(true);
-        toSave.setGender(Gender.SIRE);
-        toSave.setName("Sirion");
-        toSave.setBreed("Moody");
-        toSave.setAge(6);
-        toSave.setAdopted(false);
+        toSave = new Dog(1, "Sirion", 6, "Mudi", Gender.SIRE, LocalDateTime.now(), true, false, new BestFriend(1, null, null));
+        LocalDateTime dateTime = toSave.getLastWalk();
         assertTrue(dogRepository.findAll().isEmpty());
         Dog saved = dogRepository.save(toSave);
         assertEquals(1, dogRepository.findAll().size());
@@ -44,7 +35,7 @@ public class DogRepositoryTest {
         assertTrue(saved.isHasWaterAndFood());
         assertFalse(saved.isAdopted());
         assertEquals("Sirion", saved.getName());
-        assertEquals("Moody", saved.getBreed());
+        assertEquals("Mudi", saved.getBreed());
         assertEquals(Gender.SIRE, saved.getGender());
         assertEquals(dateTime, saved.getLastWalk());
     }
@@ -53,19 +44,11 @@ public class DogRepositoryTest {
     @Order(2)
     @Transactional
     void testUpdateDog_successfulUpdate(){
-        String ldt = "2021-08-03 15:40:00";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime dateTime = LocalDateTime.parse(ldt, formatter);
-        Dog toUpdate = new Dog();
-        toUpdate.setId(1);
-        toUpdate.setLastWalk(dateTime);
-        toUpdate.setHasWaterAndFood(true);
-        toUpdate.setGender(Gender.SIRE);
-        toUpdate.setName("Diego");
-        toUpdate.setBreed("Maltese");
-        toUpdate.setAge(11);
-        toUpdate.setAdopted(false);
-        Dog updated = dogRepository.update(toUpdate);
+        dog1 = new Dog(1, "Sirion", 6, "Mudi", Gender.SIRE, LocalDateTime.now(), true, false, new BestFriend(1, null, null));
+        dog2 = new Dog(1, "Diego", 11, "Maltese", Gender.SIRE, LocalDateTime.now(), true, false, new BestFriend(2, null, null));
+        LocalDateTime dateTime = dog2.getLastWalk();
+        dogRepository.save(dog1);
+        Dog updated = dogRepository.update(dog2);
         assertEquals(1, dogRepository.findAll().size());
         assertEquals(1, updated.getId());
         assertEquals(11, dogRepository.findById(updated.getId()).getAge());
@@ -81,26 +64,17 @@ public class DogRepositoryTest {
     @Order(3)
     @Transactional
     void testFindById_successfulFind(){
-        String ldt = "2021-08-03 15:40:00";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        LocalDateTime dateTime = LocalDateTime.parse(ldt, formatter);
-        Dog toSave = new Dog();
-        toSave.setLastWalk(dateTime);
-        toSave.setHasWaterAndFood(true);
-        toSave.setGender(Gender.SIRE);
-        toSave.setName("Diego");
-        toSave.setBreed("Maltese");
-        toSave.setAge(11);
-        toSave.setAdopted(false);
+        dog2 = new Dog(1, "Diego", 11, "Maltese", Gender.SIRE, LocalDateTime.now(), true, false, new BestFriend(2, null, null));
+        LocalDateTime dateTime = dog2.getLastWalk();
         assertTrue(dogRepository.findAll().isEmpty());
         dogRepository.save(toSave);
         Dog found = dogRepository.findById(1);
         assertEquals(1, dogRepository.findAll().size());
         assertEquals(1, found.getId());
-        assertEquals(11, dogRepository.findById(found.getId()).getAge());
+        assertEquals(11, found.getAge());
         assertTrue(found.isHasWaterAndFood());
         assertFalse(found.isAdopted());
-        assertEquals("Diego", dogRepository.findById(1).getName());
+        assertEquals("Diego", found.getName());
         assertEquals("Maltese", found.getBreed());
         assertEquals(Gender.SIRE, found.getGender());
         assertEquals(dateTime, found.getLastWalk());
@@ -157,4 +131,10 @@ public class DogRepositoryTest {
         dogRepository.walkMeBoy(1);
         assertNotEquals(originalTime, dogRepository.findById(1).getLastWalk());
     }
+    
+    /*
+    String ldt = "2021-08-03 15:40:00";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateTime = LocalDateTime.parse(ldt, formatter);
+        */
  }
